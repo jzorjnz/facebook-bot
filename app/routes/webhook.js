f02ddc35f311737e72c1cce736f83072ca0551a9
syed-zeeshan <akhtar.syedzeeshan@gmail.com>
2017-01-29 17:15:47 +0100

    Adding state

--- app/routes/webhook.js
+++ app/routes/webhook.js
@@ -109,6 +109,9 @@ sendMessage = function (sender, text) {
             if(text.toLowerCase().includes(keyword.toLowerCase())){
                 messageData = element.response;        
             }
+            if(text.toLowerCase().includes('weather')){
+                usersState[sender] = 'STATE_WEATHER';
+            }
         }, this);
     }, this);
     callSendAPI(sender, messageData);
@@ -143,22 +146,30 @@ receivedMessage = function (event) {
         var messageId = message.mid;
         text = message.text;
         var messageAttachments = message.attachments;
-        res.sendStatus(200);
     }
     else if (event.postback) {
         text = JSON.stringify(event.postback)
         console.log('postback received: ' + text);
-        res.sendStatus(200);
     }
-        
+    else{
+        // it was probably some other info that we have nothing to do at this point
+        return;
+    }   
 
     if(usersState[senderID] === 'STATE_WEATHER'){
+        usersState[senderID] = null;
         sendTextMessage(senderID, text);
     }
     else{
         console.log(' calling send message with text: ' + text);
         sendMessage(senderID, text);
     }
+    // Assume all went well.
+    //
+    // You must send back a 200, within 20 seconds, to let us know
+    // you've successfully received the callback. Otherwise, the request
+    // will time out and we will keep trying to resend.
+    res.sendStatus(200);
 }
 
 getWeather = function (callback, location) {
@@ -213,33 +224,27 @@ module.exports = function(app){
         // Make sure this is a page subscription
         if (data.object === 'page') {
         // Iterate over each entry - there may be multiple if batched
-            /*
-            data.entry.forEach(function(entry) {
-                
-            });
-            */
-            var entry = data.entry[0];
-            var pageID = entry.id;
-            var timeOfEvent = entry.time;
-            // Iterate over each messaging event
-            entry.messaging.forEach(function(event) {
-                console.log('got another entry!');
-                //receivedMessage(event);
-                /*
-                if (event.message) {
-                    receivedMessage(event);
-                } else {
-                    console.log("Webhook received unknown event: ", event);
-                }
-                */
-            });
-                
-        // Assume all went well.
-            //
-            // You must send back a 200, within 20 seconds, to let us know
-            // you've successfully received the callback. Otherwise, the request
-            // will time out and we will keep trying to resend.
             
+            data.entry.forEach(function(entry) {
+                var pageID = entry.id;
+                var timeOfEvent = entry.time;
+                // Iterate over each messaging event
+                entry.messaging.forEach(function(event) {
+                    console.log('got another entry!');
+                    receivedMessage(event);
+                    /*
+                    if (event.message) {
+                        receivedMessage(event);
+                    } else {
+                        console.log("Webhook received unknown event: ", event);
+                    }
+                    */
+                });
+            });
+            
+            
+                
+          
         }
         /*
         else{
